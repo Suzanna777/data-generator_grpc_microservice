fcb08281e2002abca1ff259ad273ff28a20decc3
“Suzanna” <americandreamdoor@gmail.com>
2024-12-21 17:30:27 -0800

    Grpc .env, Dockerfile

--- src/main/java/com/bank/DataGeneratorGrpcMicroserviceApplication.java
+++ src/main/java/com/bank/DataGeneratorGrpcMicroserviceApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DataGeneratorGrpcMicroserviceApplication {
 
     public static void main(String[] args) {
+     //   Dotenv dotenv = Dotenv.configure().load();
+     //   System.setProperty("GRPC_HOST", dotenv.get("GRPC_HOST", "localhost:50051"));
+     //   System.setProperty("BATCH_SIZE", dotenv.get("BATCH_SIZE", "100"));
+
         SpringApplication.run(DataGeneratorGrpcMicroserviceApplication.class, args);
     }
-
 }
